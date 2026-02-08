66e42b95f4c72bf75ab40ad5830a06dfff30fb90
Donna <ntang800@gmail.com>
2026-02-08 16:45:24 -0700

    add NPS part 2

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,50 @@ function parkInfoTemplate(info){
 
 const heroContent=document.querySelector(".hero-banner__content");
 heroContent.innerHTML=parkInfoTemplate(parkData);
+
+function setHeaderInfo(data){
+    const disclaimer=document.querySelector(".disclaimer>a");
+    disclaimer.href=data.url;
+    disclaimer.innerHTML=data.fullName;
+
+    document.querySelector("head>title").textContent=data.fullName;
+
+    document.querySelector(".hero-banner>img").src=data.images[0].url;
+
+    document.querySelector(".hero-banner__content").innerHTML=parkInfoTemplate(data);
+}
+
+function setParkIntro(data){
+    const introEl=document.querySelector(".intro");
+    introEl.innerHTML=`
+        <h1>${data.fullName}</h1>
+        <p>${data.description}</p>
+    `;
+}
+const parkInfoLinks =[
+    {
+        name: "Current Conditions >",
+        link: "conditions.html",
+        image: parkData.images[2].url,
+        description: "See what conditions to expect in the park before leaving on your trip."
+    }
+]
+function mediaCardTemplate(info){
+    return `
+        <article class="media-card">
+            <a href="${info.link}">
+                <img src="${info.image}" alt="">
+                <h3>${info.name}</h3>
+                </a>
+                <p>${info.description}</p>
+            </article>
+            `;
+}
+function setParkInfoLinks(data){
+    const infoEl = document.querySelector(".info");
+    const heml = data.map(mediaCardTemplate);
+    infoEl.insertAdjacentHTML("afterbegin", heml.join(""));
+}
+setHeaderInfo(parkData);
+setParkIntro(parkData);
+setParkInfoLinks(parkInfoLinks);
